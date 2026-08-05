14ec0a71f75d7d0a2c73a5bd015bc85cc9ad2872
Niharika saini <sunitasaininnt@gmail.com>
2026-08-05 23:35:45 +0530

    Implement threeSum function to find triplets

---
+++ 3Sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        int n = nums.size();
+        
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            int start = i + 1;
+            int end = n - 1;
+            
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[start], nums[end]});
+                    
+                    while (start < end && nums[start] == nums[start + 1]) {
+                        start++;
+                    }
+                    while (start < end && nums[end] == nums[end - 1]) {
+                        end--;
+                    }
+                    
+                    start++;
+                    end--;
+                } 
+                else if (sum < 0) {
+                    start++;
+                } 
+                else {
+                    end--;
+                }
+            }
+        }
+        return result;
+    }
+};
