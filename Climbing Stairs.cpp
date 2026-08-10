4dcef7cb07f885436534ab68a53e1a306a66e450
Niharika saini <sunitasaininnt@gmail.com>
2026-08-10 21:03:54 +0530

    Add climbStairs function to Solution class
    Implement climbStairs function to calculate ways to climb stairs using dynamic programming.

---
+++ Climbing Stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int first = 0,second = 1,next;
+        
+        for(int i = 0;i < n;i++){
+            next = first + second;
+            first = second;
+            second = next;
+
+            
+
+        }
+        return next;
+    }
+};
